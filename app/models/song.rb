9880d1ac2a24ad205be7766d67d2dceb8a7e5504
John Tuttle <thejwt@gmail.com>
2017-11-22 23:22:04 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -8,11 +8,11 @@ class Song < ActiveRecord::Base
   def release_year_validation
     if self.released
 
-      if self.released_year == nil
+      if self.release_year == nil
         errors.add(:release_year_validation, "If album is released, must have release year.")
       end
 
-      if self.released_year > DateTime.now.year.to_i
+      if self.release_year > DateTime.now.year.to_i
         errors.add(:release_year_validation, "Release year cannot be in the future if album is already released.")
       end
 
