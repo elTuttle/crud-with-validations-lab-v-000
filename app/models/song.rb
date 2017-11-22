d550951d50cd534ef77fb6dd5ce85a4992e16e38
John Tuttle <thejwt@gmail.com>
2017-11-22 23:17:41 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,13 +1,14 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: true
-  validates :released, inclusion: { in: %w(true false)} 
+  validates :released, inclusion: { in: %w(true false)}
 
-  def release_date_validation
+  def release_year_validation
     if self.released
-      if released_date == nil 
+      if self.released_year == nil
+        errors.add(:release_year_validation, "")
       end
     end
   end
-  
+
 end
