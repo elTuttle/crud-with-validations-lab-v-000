6767d25a4b0f203a8b7bd0768da959f58450fc78
John Tuttle <thejwt@gmail.com>
2017-11-22 23:39:17 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,27 +1,25 @@
 class SongsController < ApplicationController
-
   def index
-    
+    @songs = Song.all
   end
 
   def show
-    
+
   end
 
   def create
-    
+
   end
 
   def edit
-    
+
   end
 
   def update
-    
+
   end
 
   def destroy
-    
+
   end
-  
 end
